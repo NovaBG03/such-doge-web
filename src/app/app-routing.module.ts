626809ec4bf610c2d85fe777c8b20fbb4b3bf09e
Nikita Koynov <nikicha03@gmail.com>
2021-05-16 12:06:34 +0300

    set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MemeListComponent} from "./meme-list/meme-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MemeListComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
