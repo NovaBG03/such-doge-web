023835f6032d9769f027dd6bdae19ef47024acf8
Nikita Koynov <nikicha03@gmail.com>
2021-09-23 16:40:43 +0300

    validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MemeListComponent } from './meme-list/meme-list.component';
-import { AboutComponent } from './about/about.component';
-import { ProfileComponent } from './profile/profile.component';
-import { MemeComponent } from './meme-list/meme/meme.component';
-import { EmptyMemeComponent } from './meme-list/empty-meme/empty-meme.component';
-import { MenuComponent } from './menu/menu.component';
-import { MemeFormComponent } from './meme-form/meme-form.component';
-import { AuthComponent } from './auth/auth.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {MemeListComponent} from './meme-list/meme-list.component';
+import {AboutComponent} from './about/about.component';
+import {ProfileComponent} from './profile/profile.component';
+import {MemeComponent} from './meme-list/meme/meme.component';
+import {EmptyMemeComponent} from './meme-list/empty-meme/empty-meme.component';
+import {MenuComponent} from './menu/menu.component';
+import {MemeFormComponent} from './meme-form/meme-form.component';
+import {AuthComponent} from './auth/auth.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -30,9 +31,11 @@ import { AuthComponent } from './auth/auth.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {map} from "rxjs/operators";
 import {Subscription} from "rxjs";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
@@ -12,9 +13,12 @@ export class AuthComponent implements OnInit, OnDestroy {
   isRegister = true;
   imageSrc = '';
 
+  authForm!: FormGroup;
+
   private urlSub!: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
     this.urlSub = this.route.url
@@ -26,6 +30,7 @@ export class AuthComponent implements OnInit, OnDestroy {
         } else {
           this.imageSrc = '/assets/images/kawai_doge.jpg';
         }
+        this.initAuthForm();
       });
   }
 
@@ -39,5 +44,75 @@ export class AuthComponent implements OnInit, OnDestroy {
     }
 
     return '#2B2935';
+  }
+
+  getControl(controlName: string): FormControl {
+    return this.authForm.get(controlName) as FormControl;
+  }
+
+  private initAuthForm() {
+    this.authForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(36)
+      ]),
+      passwords: new FormGroup({
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(50),
+          this.hasDigitsValidator(),
+          this.hasAlphabeticCharacters()
+        ])
+      })
+    });
+
+    if (this.isRegister) {
+      this.authForm.addControl('email',
+        new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(254),
+          Validators.email
+        ]));
+
+      const passwordsGroup = this.authForm.get('passwords') as FormGroup;
+
+      passwordsGroup.setValidators(this.isPasswordConfirmedValidator());
+
+      passwordsGroup.addControl('confirmed',
+        new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(50),
+          this.hasDigitsValidator(),
+          this.hasAlphabeticCharacters()
+        ]));
+    }
+  }
+
+  private hasDigitsValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const hasDigits = control.value && /\d/.test(control.value);
+      return hasDigits ? null : {hasDigit: {value: control.value}};
+    }
+  }
+
+  private hasAlphabeticCharacters(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const hasAlphabeticCharacters = control.value && /[a-z]/.test(control.value);
+      return hasAlphabeticCharacters ? null : {hasAlphabeticCharacters: {value: control.value}};
+    }
+  }
+
+  private isPasswordConfirmedValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.get('password')?.value;
+      const confirmPassword = control.get('confirmed')?.value;
+
+      const isPasswordConfirmed = password === confirmPassword;
+      return isPasswordConfirmed ? null : {isPasswordConfirmed: {value: control.value}};
+    }
   }
 }
