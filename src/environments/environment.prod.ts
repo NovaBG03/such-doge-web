a1c2d50c121551fe493a69e1d2c819dbe64953ed
Nikita Koynov <nikicha03@gmail.com>
2021-11-19 21:13:55 +0200

    env prop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  suchDogeApi: 'https://suchdoge-api.herokuapp.com',
+  authPrefix: "Bearer ",
+  authHeader: "Authorization",
+  authTokenKey: 'dogeAuthToken',
+  preferredThemeKey: 'dogePreferredTheme',
+  defaultMemePageSize: 8,
+  minUsernameLength: 3,
+  maxUsernameLength: 36,
+  minEmailLength: 3,
+  maxEmailLength: 254,
+  minPasswordLength: 6,
+  maxPasswordLength: 50
 };
